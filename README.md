2e912120dab70540360ecd81f88c96445e087ede
redraw <redraw@sdf.org>
2025-09-12 18:36:33 -0300

    agrego salario minimo vital y móvil

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Fuentes:
 - https://www.datos.gob.ar/dataset/sspm-remuneracion-imponible-promedio-trabajadores-estables-ripte
 - https://www.datos.gob.ar/dataset/sspm-valores-canasta-basica-alimentos-canasta-basica-total
+- https://www.datos.gob.ar/dataset/sspm-salario-minimo-vital-movil-pesos-corrientes
--- main.py
+++ main.py
@@ -7,6 +7,13 @@ ripte = pd.read_csv(
     parse_dates=["indice_tiempo"],
     index_col="indice_tiempo",
 )
+
+smvm = pd.read_csv(
+    "https://infra.datos.gob.ar/catalog/sspm/dataset/57/distribution/57.1/download/indice-salario-minimo-vital-movil-valores-mensuales-pesos-corrientes-desde-1988.csv",
+    parse_dates=["indice_tiempo"],
+    index_col="indice_tiempo",
+)
+
 canasta = pd.read_csv(
     "https://infra.datos.gob.ar/catalog/sspm/dataset/150/distribution/150.1/download/valores-canasta-basica-alimentos-canasta-basica-total-mensual-2016.csv",
     parse_dates=["indice_tiempo"],
@@ -20,11 +27,19 @@ canasta_caba = pd.read_csv(
 )
 
 pdc = pd.DataFrame()
-pdc["CBA"] = ripte.ripte / canasta.canasta_basica_alimentaria
-pdc["CBT"] = ripte.ripte / canasta.canasta_basica_total
-pdc["CBA_CABA"] = ripte.ripte / canasta_caba.canasta_basica_alimentaria
-pdc["CBT_CABA"] = ripte.ripte / canasta_caba.canasta_basica_total
+pdc["RIPTE_CBA"] = ripte.ripte / canasta.canasta_basica_alimentaria
+pdc["RIPTE_CBT"] = ripte.ripte / canasta.canasta_basica_total
+pdc["RIPTE_CBA_CABA"] = ripte.ripte / canasta_caba.canasta_basica_alimentaria
+pdc["RIPTE_CBT_CABA"] = ripte.ripte / canasta_caba.canasta_basica_total
+pdc["SMVM_CBA"] = smvm.salario_minimo_vital_movil_mensual / canasta.canasta_basica_alimentaria
+pdc["SMVM_CBT"] = smvm.salario_minimo_vital_movil_mensual / canasta.canasta_basica_total
+pdc["SMVM_CBA_CABA"] = smvm.salario_minimo_vital_movil_mensual / canasta_caba.canasta_basica_alimentaria
+pdc["SMVM_CBT_CABA"] = smvm.salario_minimo_vital_movil_mensual / canasta_caba.canasta_basica_total
+pdc = pdc.dropna(how="all")
+
+# export JSON
 pdc.to_json("data.json")
 
-fig = pdc.loc["2013":].plot(log_y=True)
+# export plot
+fig = pdc.plot(log_y=True)
 fig.write_html("index.html")
